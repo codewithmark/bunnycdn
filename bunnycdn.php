e062d679448890cc3c7d86770a18d10f387a5719
Code With Mark <mark2kumar@gmail.com>
2019-03-29 17:25:03 -0400

    Added file log stats for the api

--- bunnycdn.php
+++ bunnycdn.php
@@ -647,6 +647,9 @@ class BunnyCDN
 		die();
 	}
 
+
+
+
 	public function Billing()
 	{	
 		/*
@@ -841,7 +844,9 @@ class BunnyCDN
 		die();
 	}
 
- 
+ 	
+
+
 	public function PutFile($local_upload_file_path ='',  $storage_zone_path='', $storage_zone_file_path='' )
 	{
 		/*
@@ -1144,6 +1149,134 @@ class BunnyCDN
 	    }
 	}
 
+	public function Logs( $zone_id = '', $log_date = '')
+	{	
+		/*
+			will get log for the zone
+		*/	
+
+		if( !$this->api_key_account)
+		{
+			return array('status' =>'error' ,'code' =>'api_key_account' ,'msg'=> 'missing acount api key');
+			die();
+		} 
+
+		if(!$log_date)
+		{
+			$date = new DateTime();
+
+			//today minus 1 day... if today(2019-03-29), then date is: 2019-03-28
+			$date = $date->modify("-1 day");
+			$date = $date->format('m-d-y');
+			$log_dt = $date;
+		}
+		else if( $log_date)
+		{
+			$date = new DateTime($log_date);
+			$date = $date->format('m-d-y');
+			$log_dt = $date;
+		}
+ 
+		$key =  $this->api_key_account;
+
+		
+
+		//$api_url = 'https://logging.bunnycdn.com/{mm}-{dd}-{yy}/{pull_zone_id}.log';
+		$api_url = 'https://logging.bunnycdn.com/'.$log_dt.'/'.$zone_id.'.log';
+
+		$get_header = $this->create_header($key);
+		 
+	    $api_call = $this->run( array('call_method' => 'GET', 'api_url' => $api_url,'header' => $get_header , ));
+		
+		if($api_call['http_code'] !=200 )
+		{
+			//error message
+			$request_array =  json_decode(json_encode($api_call['data']));
+			$result = array
+			(	
+				"status" => 'error',
+				"http_code"=>$api_call['http_code'],
+				"msg" => ($request_array) , 			 
+			);
+			return $result;
+			//die();
+		}
+		else if(strlen($api_call['data']) <1 )
+		{
+			$result = array
+			(	
+				"status" => 'error',
+				"http_code"=> 800,				
+				'msg'=> 'Ran successfully but no log data returned for the current selection.',
+			);
+			return $result;
+			//die();
+		}
+		else if($api_call['http_code'] == 200)
+		{
+
+			//convert/parse it to line break
+			$t1 = explode("\n",$api_call['data']);
+
+			$a1 = array();
+
+			foreach ($t1 as $v1) 
+			{
+				if(isset($v1) && strlen($v1) > 0)
+				{
+					//parse "|"
+					$t2 = explode("|", $v1);
+
+					//divide it by 1000 to convert it to php unix time
+					$time = round($t2[2] /1000, 0);
+
+					$a2 = array(
+						'cache_hit' => $t2[0],
+
+						'status' => $t2[1],
+						'status_code' => $this->get_http_status_code($t2[1]),
+
+						'time_js' => $t2[2]*1,
+
+						'time_unix' => $time,						
+						'time_dttm' => date('Y-m-d H:i:s',  $time),
+						'time_dt' => date('Y-m-d',  $time),
+						
+						'bytes' => $t2[3],
+						'bytes_format' => $this->format_bytes($t2[3]),
+
+						'zone_id' => $t2[4],
+						'remote_ip' => $t2[5],
+
+						'referer_url' => strlen($t2[6]) > 1 ? ($t2[6]) : 'direct',
+						'referer_url_raw' =>  $t2[6],
+
+
+						'file_url' => $t2[7],
+
+						'cdn_datacenter_loc' => $t2[8],						
+
+						'user_agent' => $t2[9],
+						'request_id' => $t2[10],
+
+						'country' => $t2[11],						
+						'country_name' => $this->get_country_name($t2[11]),
+					);	 
+					array_push($a1, $a2);
+				}			
+			}
+
+			$get_stats = $this->Account($key)->GetZone($zone_id)['zone_smry'][0];
+
+			return array(
+				'status' => 'success',
+				"log" => $a1, 
+				'zone_current_monthly_bandwidth_used' => $get_stats['monthly_bandwidth_used'],
+				'zone_name'=> $get_stats['zone_name'], 
+			);
+		}
+		 
+	}
 
 	//--->process functions > start
 
@@ -1196,6 +1329,8 @@ class BunnyCDN
 
 
 	//--->private functions > start
+
+	
 
 	private  function fix_url($url ='')
 	{
@@ -1275,6 +1410,356 @@ class BunnyCDN
 		}
  
 	    return $file_path;
+	}
+
+
+	private  function get_country_name($country_code)
+	{
+		$country_name = array(
+			"A1" => "Anonymous Proxy",
+			"A2" => "Satellite Provider",
+			"O1" => "Other Country",
+			"AD" => "Andorra",
+			"AE" => "United Arab Emirates",
+			"AF" => "Afghanistan",
+			"AG" => "Antigua and Barbuda",
+			"AI" => "Anguilla",
+			"AL" => "Albania",
+			"AM" => "Armenia",
+			"AO" => "Angola",
+			"AP" => "Asia/Pacific Region",
+			"AQ" => "Antarctica",
+			"AR" => "Argentina",
+			"AS" => "American Samoa",
+			"AT" => "Austria",
+			"AU" => "Australia",
+			"AW" => "Aruba",
+			"AX" => "Aland Islands",
+			"AZ" => "Azerbaijan",
+			"BA" => "Bosnia and Herzegovina",
+			"BB" => "Barbados",
+			"BD" => "Bangladesh",
+			"BE" => "Belgium",
+			"BF" => "Burkina Faso",
+			"BG" => "Bulgaria",
+			"BH" => "Bahrain",
+			"BI" => "Burundi",
+			"BJ" => "Benin",
+			"BL" => "Saint Bartelemey",
+			"BM" => "Bermuda",
+			"BN" => "Brunei Darussalam",
+			"BO" => "Bolivia",
+			"BQ" => "Bonaire, Saint Eustatius and Saba",
+			"BR" => "Brazil",
+			"BS" => "Bahamas",
+			"BT" => "Bhutan",
+			"BV" => "Bouvet Island",
+			"BW" => "Botswana",
+			"BY" => "Belarus",
+			"BZ" => "Belize",
+			"CA" => "Canada",
+			"CC" => "Cocos (Keeling) Islands",
+			"CD" => "Congo, The Democratic Republic of the",
+			"CF" => "Central African Republic",
+			"CG" => "Congo",
+			"CH" => "Switzerland",
+			"CI" => "Cote d'Ivoire",
+			"CK" => "Cook Islands",
+			"CL" => "Chile",
+			"CM" => "Cameroon",
+			"CN" => "China",
+			"CO" => "Colombia",
+			"CR" => "Costa Rica",
+			"CU" => "Cuba",
+			"CV" => "Cape Verde",
+			"CW" => "Curacao",
+			"CX" => "Christmas Island",
+			"CY" => "Cyprus",
+			"CZ" => "Czech Republic",
+			"DE" => "Germany",
+			"DJ" => "Djibouti",
+			"DK" => "Denmark",
+			"DM" => "Dominica",
+			"DO" => "Dominican Republic",
+			"DZ" => "Algeria",
+			"EC" => "Ecuador",
+			"EE" => "Estonia",
+			"EG" => "Egypt",
+			"EH" => "Western Sahara",
+			"ER" => "Eritrea",
+			"ES" => "Spain",
+			"ET" => "Ethiopia",
+			"EU" => "Europe",
+			"FI" => "Finland",
+			"FJ" => "Fiji",
+			"FK" => "Falkland Islands (Malvinas)",
+			"FM" => "Micronesia, Federated States of",
+			"FO" => "Faroe Islands",
+			"FR" => "France",
+			"GA" => "Gabon",
+			"GB" => "United Kingdom",
+			"GD" => "Grenada",
+			"GE" => "Georgia",
+			"GF" => "French Guiana",
+			"GG" => "Guernsey",
+			"GH" => "Ghana",
+			"GI" => "Gibraltar",
+			"GL" => "Greenland",
+			"GM" => "Gambia",
+			"GN" => "Guinea",
+			"GP" => "Guadeloupe",
+			"GQ" => "Equatorial Guinea",
+			"GR" => "Greece",
+			"GS" => "South Georgia and the South Sandwich Islands",
+			"GT" => "Guatemala",
+			"GU" => "Guam",
+			"GW" => "Guinea-Bissau",
+			"GY" => "Guyana",
+			"HK" => "Hong Kong",
+			"HM" => "Heard Island and McDonald Islands",
+			"HN" => "Honduras",
+			"HR" => "Croatia",
+			"HT" => "Haiti",
+			"HU" => "Hungary",
+			"ID" => "Indonesia",
+			"IE" => "Ireland",
+			"IL" => "Israel",
+			"IM" => "Isle of Man",
+			"IN" => "India",
+			"IO" => "British Indian Ocean Territory",
+			"IQ" => "Iraq",
+			"IR" => "Iran, Islamic Republic of",
+			"IS" => "Iceland",
+			"IT" => "Italy",
+			"JE" => "Jersey",
+			"JM" => "Jamaica",
+			"JO" => "Jordan",
+			"JP" => "Japan",
+			"KE" => "Kenya",
+			"KG" => "Kyrgyzstan",
+			"KH" => "Cambodia",
+			"KI" => "Kiribati",
+			"KM" => "Comoros",
+			"KN" => "Saint Kitts and Nevis",
+			"KP" => "Korea, Democratic People's Republic of",
+			"KR" => "Korea, Republic of",
+			"KW" => "Kuwait",
+			"KY" => "Cayman Islands",
+			"KZ" => "Kazakhstan",
+			"LA" => "Lao People's Democratic Republic",
+			"LB" => "Lebanon",
+			"LC" => "Saint Lucia",
+			"LI" => "Liechtenstein",
+			"LK" => "Sri Lanka",
+			"LR" => "Liberia",
+			"LS" => "Lesotho",
+			"LT" => "Lithuania",
+			"LU" => "Luxembourg",
+			"LV" => "Latvia",
+			"LY" => "Libyan Arab Jamahiriya",
+			"MA" => "Morocco",
+			"MC" => "Monaco",
+			"MD" => "Moldova, Republic of",
+			"ME" => "Montenegro",
+			"MF" => "Saint Martin",
+			"MG" => "Madagascar",
+			"MH" => "Marshall Islands",
+			"MK" => "Macedonia",
+			"ML" => "Mali",
+			"MM" => "Myanmar",
+			"MN" => "Mongolia",
+			"MO" => "Macao",
+			"MP" => "Northern Mariana Islands",
+			"MQ" => "Martinique",
+			"MR" => "Mauritania",
+			"MS" => "Montserrat",
+			"MT" => "Malta",
+			"MU" => "Mauritius",
+			"MV" => "Maldives",
+			"MW" => "Malawi",
+			"MX" => "Mexico",
+			"MY" => "Malaysia",
+			"MZ" => "Mozambique",
+			"NA" => "Namibia",
+			"NC" => "New Caledonia",
+			"NE" => "Niger",
+			"NF" => "Norfolk Island",
+			"NG" => "Nigeria",
+			"NI" => "Nicaragua",
+			"NL" => "Netherlands",
+			"NO" => "Norway",
+			"NP" => "Nepal",
+			"NR" => "Nauru",
+			"NU" => "Niue",
+			"NZ" => "New Zealand",
+			"OM" => "Oman",
+			"PA" => "Panama",
+			"PE" => "Peru",
+			"PF" => "French Polynesia",
+			"PG" => "Papua New Guinea",
+			"PH" => "Philippines",
+			"PK" => "Pakistan",
+			"PL" => "Poland",
+			"PM" => "Saint Pierre and Miquelon",
+			"PN" => "Pitcairn",
+			"PR" => "Puerto Rico",
+			"PS" => "Palestinian Territory",
+			"PT" => "Portugal",
+			"PW" => "Palau",
+			"PY" => "Paraguay",
+			"QA" => "Qatar",
+			"RE" => "Reunion",
+			"RO" => "Romania",
+			"RS" => "Serbia",
+			"RU" => "Russian Federation",
+			"RW" => "Rwanda",
+			"SA" => "Saudi Arabia",
+			"SB" => "Solomon Islands",
+			"SC" => "Seychelles",
+			"SD" => "Sudan",
+			"SE" => "Sweden",
+			"SG" => "Singapore",
+			"SH" => "Saint Helena",
+			"SI" => "Slovenia",
+			"SJ" => "Svalbard and Jan Mayen",
+			"SK" => "Slovakia",
+			"SL" => "Sierra Leone",
+			"SM" => "San Marino",
+			"SN" => "Senegal",
+			"SO" => "Somalia",
+			"SR" => "Suriname",
+			"SS" => "South Sudan",
+			"ST" => "Sao Tome and Principe",
+			"SV" => "El Salvador",
+			"SX" => "Sint Maarten",
+			"SY" => "Syrian Arab Republic",
+			"SZ" => "Swaziland",
+			"TC" => "Turks and Caicos Islands",
+			"TD" => "Chad",
+			"TF" => "French Southern Territories",
+			"TG" => "Togo",
+			"TH" => "Thailand",
+			"TJ" => "Tajikistan",
+			"TK" => "Tokelau",
+			"TL" => "Timor-Leste",
+			"TM" => "Turkmenistan",
+			"TN" => "Tunisia",
+			"TO" => "Tonga",
+			"TR" => "Turkey",
+			"TT" => "Trinidad and Tobago",
+			"TV" => "Tuvalu",
+			"TW" => "Taiwan",
+			"TZ" => "Tanzania, United Republic of",
+			"UA" => "Ukraine",
+			"UG" => "Uganda",
+			"UM" => "United States Minor Outlying Islands",
+			"US" => "United States",
+			"UY" => "Uruguay",
+			"UZ" => "Uzbekistan",
+			"VA" => "Holy See (Vatican City State)",
+			"VC" => "Saint Vincent and the Grenadines",
+			"VE" => "Venezuela",
+			"VG" => "Virgin Islands, British",
+			"VI" => "Virgin Islands, U.S.",
+			"VN" => "Vietnam",
+			"VU" => "Vanuatu",
+			"WF" => "Wallis and Futuna",
+			"WS" => "Samoa",
+			"YE" => "Yemen",
+			"YT" => "Mayotte",
+			"ZA" => "South Africa",
+			"ZM" => "Zambia",
+			"ZW" => "Zimbabwe",
+		);
+		//return $country_name[$country_code];	
+		foreach ($country_name as $k1 => $v1) 
+		{
+			if($k1 == $country_code)
+			{
+				return $v1;
+			}			
+		} 	 
+	}
+
+	private function get_http_status_code($code)
+	{
+		$status_code = array(
+			// 1XX
+		    "100" => "Continue",
+		    "101" => "Switching Protocols",
+		    "102" => "Processing",
+		    "103" => "Early Hints",
+		    // 2XX
+		    "200" => "OK",
+		    "201" => "Created",
+		    "202" => "Accepted",
+		    "203" => "Non-Authoritative Information",
+		    "204" => "No Content",
+		    "206" => "Partial Content",
+		    "207" => "Multi-Status",
+		    "208" => "Already Reported",
+		    "226" => "IM Used",
+		    // 3XX
+		    "300" => "Multiple Choices",
+		    "301" => "Moved Permanently",
+		    "302" => "Found",
+		    "303" => "See Other",
+		    "304" => "Not Modified",
+		    "305" => "Use Proxy",
+		    "306" => "Switch Proxy",
+		    "307" => "Temporary Redirect",
+		    "308" => "Permanent Redirect",
+		    // 4XX
+		    "400" => "Bad Request",
+		    "401" => "Unauthorized",
+		    "402" => "Payment Required",
+		    "403" => "Forbidden",
+		    "404" => "Not Found",
+		    "405" => "Method Not Allowed",
+		    "406" => "Not Acceptable",
+		    "407" => "Proxy Authentication Required",
+		    "408" => "Request Timeout",
+		    "409" => "Conflict",
+		    "410" => "Gone",
+		    "411" => "Length Required",
+		    "412" => "Precondition Failed",
+		    "413" => "Payload Too Large",
+		    "414" => "URI Too Long",
+		    "415" => "Unsupported Media Type",
+		    "416" => "Range Not Satisfiable",
+		    "417" => "Expectation Failed",
+		    "418" => "I'm a teapot",
+		    "421" => "Misdirected Request",
+		    "422" => "Unprocessable Entity",
+		    "423" => "Locked",
+		    "424" => "Failed Dependency",
+		    "426" => "Upgrade Required",
+		    "428" => "Precondition Required",
+		    "429" => "Too Many Requests",
+		    "431" => "Request Header Fields Too Large",
+		    "451" => "Unavailable For Legal Reasons",
+
+		    // 5XX
+		    "500" => "Internal Server Error",
+		    "501" => "Not Implemented",
+		    "502" => "Bad Gateway",
+		    "503" => "Service Unavailable",
+		    "504" => "Gateway Timeout",
+		    "505" => "HTTP Version Not Supported",
+		    "506" => "Variant Also Negotiates",
+		    "507" => "Insufficient Storage",
+		    "508" => "Loop Detected",
+		    "510" => "Not Extended",
+		    "511" => "Network Authentication Required",
+		);
+		
+		foreach ($status_code as $k1 => $v1) 
+		{
+			if($k1 == $code)
+			{
+				return $v1;
+			}			
+		} 		
 	}
 
 	
